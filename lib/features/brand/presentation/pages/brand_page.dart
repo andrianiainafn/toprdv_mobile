848321ce184286b4b8e458198f4031d9c1170b01
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2025-01-09 14:33:34 +0300

    [ADD] add brand presentation

--- lib/features/brand/presentation/pages/brand_page.dart
+++ lib/features/brand/presentation/pages/brand_page.dart
@@ -1,5 +1,11 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_template_project/features/brand/presentation/bloc/brand_bloc.dart';
+import 'package:my_template_project/features/brand/presentation/bloc/related_users_bloc.dart';
+import 'package:my_template_project/features/brand/presentation/widget/brand_form_widget.dart';
+
+import '../../../../injection_container.dart';
 
 @RoutePage()
 class BrandPage extends StatelessWidget {
@@ -7,6 +13,21 @@ class BrandPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text("Modifer les enseignes a vuivre",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold)),
+        buildBody(context)
+      ],
+    );
   }
+}
+
+MultiBlocProvider buildBody(BuildContext context) {
+  return MultiBlocProvider(providers: [
+      BlocProvider(create: (context)=> sl<BrandBloc>()),
+      BlocProvider(create: (context)=> sl<RelatedUsersBloc>())
+  ], child: Container(
+    margin: EdgeInsets.symmetric(vertical: 0,horizontal: 2),
+    child: BrandFormWidget(),
+  ));
 }
---
+++ lib/features/brand/presentation/widget/brand_form_widget.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_template_project/core/global/action_button.dart';
+import 'package:my_template_project/features/brand/domain/entity/related_brand.dart';
+import '../../domain/entity/brand.dart';
+import '../bloc/brand_bloc.dart';
+import '../bloc/related_users_bloc.dart';
+
+
+
+// État du widget
+class BrandFormWidget extends StatefulWidget {
+  const BrandFormWidget({super.key});
+
+  @override
+  State<BrandFormWidget> createState() => _BrandFormWidgetState();
+}
+
+class _BrandFormWidgetState extends State<BrandFormWidget> {
+  List<RelatedBrand> brandSelected = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Équivalent du useEffect pour charger les données initiales
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<BrandBloc>().add(GetBrandEvent());
+      context.read<RelatedUsersBloc>().add(GetRelatedUser());
+    });
+  }
+  void onSubmitForm(){
+
+  }
+
+  bool checkBrandAlreadySelected(Brand brand) {
+    return brandSelected.any((item) => item.id == brand.id);
+  }
+
+  void updateBrandSelected(Brand brand) {
+    setState(() {
+      final brandAsRelatedUser = RelatedBrand(
+        id: brand.id,
+        brandName: brand.value,
+      );
+
+      if (checkBrandAlreadySelected(brand)) {
+        brandSelected.removeWhere((b) => b.id == brand.id);
+      } else {
+        brandSelected.add(brandAsRelatedUser);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BrandBloc, BrandState>(
+      builder: (context, brandState) {
+        if (brandState is LoadingBrand) {
+          return const CircularProgressIndicator();
+        }
+
+        if (brandState is LoadedBrand) {
+          return BlocListener<RelatedUsersBloc, RelatedUsersState>(
+            listener: (context, relatedUsersState) {
+              if (relatedUsersState is RelatedUsersLoaded) {
+                setState(() {
+                  brandSelected = relatedUsersState.relatedUsers;
+                });
+              }
+            },
+            child: Column(
+              children: [
+                Wrap(
+                  spacing: 8.0,
+                  runSpacing: 8.0,
+                  children: brandState.brand.map((item) =>
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.48,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                item.value,
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Checkbox(
+                              value: checkBrandAlreadySelected(item),
+                              onChanged: (_) => updateBrandSelected(item),
+                              activeColor: Colors.green.shade600,
+                            ),
+                          ],
+                        ),
+                      ),
+                  ).toList(),
+                ),
+                ActionButton(onSubmitForm: onSubmitForm, title: "Sauvegarder")
+              ],
+            ),
+          );
+        }
+
+        if (brandState is ErrorBrand) {
+          return Text('Erreur: ${(brandState as ErrorBrand).message}');
+        }
+        return const SizedBox.shrink();
+      },
+    );
+  }
+}
