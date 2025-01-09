fff2fa885ca11e2e2abc52641727a4170bd3a203
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2025-01-09 14:39:36 +0300

    [ADD] add login form and some global widget

---
+++ lib/features/profile/presentation/widget/logout_widget.dart
@@ -0,0 +1,31 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../auth/presentation/bloc/auth_bloc.dart';
+
+
+class LogoutWidget extends StatelessWidget {
+  const LogoutWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthBloc,AuthState>(builder: (context,state){
+      return TextButton(onPressed: (){
+        BlocProvider.of<AuthBloc>(context).add(LogoutEvent());
+      }, child: Text("Se deconnecter",style: TextStyle(fontSize: 16),));
+    },
+        listener: (context, state) {
+          if (state is Loaded) {
+            context.router.replaceNamed("/login");
+          }
+          if (state is ErrorState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.message)),
+            );
+          }
+        }
+
+    );
+  }
+}
